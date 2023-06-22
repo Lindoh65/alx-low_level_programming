5a7af9431dc855d1faf894d18b581cfe36d0c5e3
Lindoh65 <lindokuhlesukazi24@gmail.com>
2023-06-22 10:06:41 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writers the character c to shodout 
+ * @c: The character to print 
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ .
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writers the character c to shodout 
+ * @c: The character to print 
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ .
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writers the character c to shodout 
+ * @c: The character to print 
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ .
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
